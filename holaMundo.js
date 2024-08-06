f18053f2c2bad49b1535db71622daee5234ee489
David Chaves <dfchaves17@gmail.com>
2024-08-06 06:49:57 -0500

    Create new variables types in JavaScript

--- holaMundo.js
+++ holaMundo.js
@@ -1,2 +1,17 @@
 let nombre = 'David';
 console.log('Hola Mundo', nombre);
+
+const pi = 3.1416;
+let bolleanos = true;
+
+if (bolleanos) {
+    console.log('La variable es verdadera.');
+} else {
+    console.log('La variables es falsa');
+}
+
+const arreglos = ['Coco', 'Piña', 'Pera', 'Banana', 'Manzana'];
+
+arreglos.forEach((fruta)=>{
+    console.log('Esta es la fruta ', fruta);
+});
